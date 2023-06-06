380c53a6beb09cd1ac44fdb6b6301be7628608b1
FernandaH2610 <fernanda.hdz2601@gmail.com>
2023-06-06 17:39:50 -0600

    :bug::fire: Bugs reparados

--- Proyecto1.cpp
+++ Proyecto1.cpp
@@ -5,58 +5,23 @@ using namespace std;
 
 void menuPrincipal();
 void evaluarMenu1(int opcion1);
-int obtenerNumeroMenu1();
+int obtener1();
 
 int main() {
-    int numero = obtenerNumeroMenu1();
-    evaluarMenu1(numero);
     menuPrincipal();
     return 0;
 }
 
-/*OBTENCION DE DATOS
-    Son modulos con valor, sirven para obtener un dato y devolverlos a modulos vacios*/
-
-int obtenerNumeroMenu1(){
-    int numero;
-    cout << "Ingresa un numero: ";
-    cin >> numero;
-
-    return numero;
+int obtener1(){
+    int opcion1;
+    cout << "Ingrese la opcion que desea ejecutar: \n";
+    cin >> opcion1;
+    return opcion1;
 }
 
-/*VALIDACION DE DATOS
-    Son modulos con vacios, sirven para obtener contener informacion, sin regresar información
-    se usan para diferentes cosas, impresion de textos*/
-
-void evaluarMenu1(int opcion1){
-    if (opcion1 >= 1 && opcion1 <= 3)
-    {
-        cout << "Opcion Valida, en seguida tienes tu respuesta" << endl;
-        
-    }        
-    else
-        {
-            if (opcion1 == 4){
-
-                cout << "Gracias por usar el programa." << endl;
-        
-            } 
-            else{
-                
-                cout << "Opcion Invalida, por favor introduce una opcion valida :D";
-
-            }
-            
-        }
-    
-}
 
 void menuPrincipal(){
-    int opcion;
 
-    do
-    {
         system("cls");
         cout <<"\n\n\t\t\t\t Menu Principal" << endl;
         cout <<"\t\t\t---------------------" << endl;
@@ -64,10 +29,9 @@ void menuPrincipal(){
         cout <<"\n\t2. Menu De Calculo De Un Perimetro" << endl;
         cout <<"\n\t3. Menu De Calculo De cacular un perimetro" << endl;
         cout <<"\n\t4. Salir del programa" << endl;
-
-        cout <<"\nIngrese Una Opcion del menu" << endl;
-        cin >> opcion;
-        evaluarMenu1(opcion);
+        int opcion = obtener1();
+    do
+    {
         switch (opcion) {
             case 1:
                 cout <<"No puedo Marta" << endl;
@@ -83,6 +47,6 @@ void menuPrincipal(){
                 break;
         }
 
-    } while (opcion != 0);
+    } while (opcion <= 1 && opcion >= 4);
     
 }
